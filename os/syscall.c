4e1a029ba52f5e10bcc6a189de808102b9c58b19
alanazi8 <alanazi8@msu.edu>
2026-03-26 17:10:19 +0000

    moving proj1 stuff

--- os/syscall.c
+++ os/syscall.c
@@ -52,6 +52,28 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz) // TODO: implement sys_gettimeofd
 /*
 * LAB1: you may need to define sys_task_info here
 */
+int sys_task_info(struct TaskInfo *ti)
+
+
+{
+
+
+	struct proc *p = curr_proc();
+
+
+	ti->status = Running;
+
+
+	ti->time = (get_cycle() - p->start_time) / (CPU_FREQ / 1000);
+
+
+	memmove(ti->syscall_times, p->syscall_times, sizeof(p->syscall_times));
+
+
+	return 0;
+
+
+}
 
 extern char trap_page[];
 
@@ -66,6 +88,13 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	if (id >= 0 && id < MAX_SYSCALL_NUM) {
+
+
+		curr_proc()->syscall_times[id]++;
+
+
+	}
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], args[1], args[2]);
@@ -82,6 +111,13 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	case SYS_task_info:
+
+
+		ret = sys_task_info((struct TaskInfo *)args[0]);
+
+
+		break;
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
